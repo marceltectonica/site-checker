ff2318dd8cc13d6cd506e12fc1945e728a12117a
marcelsafont <marcel.safont1983@gmail.com>
2023-02-13 06:48:52 +0100

    lighthouse batch working

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { XMLParser} from 'fast-xml-parser';
-import lighthouse from 'lighthouse';
-import chromeLauncher from 'chrome-launcher';
-import { spawn } from 'node:child_process';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { execSync } from 'node:child_process'
+import fetch from 'node-fetch';
 
 const argv = yargs(hideBin(process.argv)).argv
 
@@ -13,39 +12,22 @@ async function checker(){
     console.log('url is required')
     return false
   }
-  let results = [];
   
   const parser = new XMLParser();
   const url = argv.url
-
   const response = await fetch(url)
   const xml = await response.text()
   let urlsXml = parser.parse(xml);
-  const urlsList = urlsXml.urlset.url.map(item => item.loc).slice(1,2);
-
-  const result = urlsList.map(async url => {
-    let chrome = await chromeLauncher.launch({'--headless': true});
-    const options = {output: 'html', onlyCategories: ['performance'], port: chrome.port};
-    const runnerResult = await lighthouse(url, options);
-    // console.log('Report is done for', runnerResult.lhr.finalDisplayedUrl);
-    // console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
-    results.push({url, score: runnerResult.lhr.categories.performance.score})
-    await chrome.kill();
-  })
-
-  const resultTest = await Promise.all(result)
-  console.log(results)
-  
-  // try {
-  //     execSync(`lighthouse-batch -s ${urlsList} `); // Executes this on the command line to run the performance test
-  // }
-  // catch(err) {
-  //   console.log(err)
-  //     console.log(`Performance test ${runs + 1} failed`); // If Lighthouse happens to fail it'll log this to the console and log the error message
-  //   return
-  // }
-  await console.log(`All finished`);
-
+  const urlsList = urlsXml.urlset.url.map(item => item.loc).slice(1,3);  
+  try {
+      execSync(`lighthouse-batch -s ${urlsList}`); // Executes this on the command line to run the performance test
+      console.log('done')
+  }
+  catch(err) {
+    console.log(err)
+      console.log(`Performance test failed`); // If Lighthouse happens to fail it'll log this to the console and log the error message
+    return
+  }
 }
 
 checker();
